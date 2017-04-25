ac00f930e9af4ecce70beb7b97c29089b35420c5
Victor <victorherrerocazurro@gmail.com>
2017-04-25 14:47:52 +0200

    Resolucion ejercicio 3

--- 03-Ejercicio/src/com/ejemplo/Prueba.scala
+++ 03-Ejercicio/src/com/ejemplo/Prueba.scala
@@ -1,33 +1,6 @@
 package com.ejemplo
 
 object Prueba extends App {
-
-  //Definiciones
-  
-  def retornaUnReal(valor : Double): Double = {valor}
-  
-  def retornaUnInt(edad: Int) = edad
-  
-  def retornaUnString = {"Hola!"}
-  
-  def retornaUnString(saludo : String) = {saludo}
-  
-  def retornaUnString(saludo : String, edad : Int) = {saludo + edad}
-  
-  def noRetornaNada {"Hola!"}
-  
-  //Invocaciones
-  
-  retornaUnString
-  
-  var entero = Prueba retornaUnInt 12
-  
-  var booleano = (Prueba retornaUnInt 12) != 13
-  
-  var texto = Prueba retornaUnString "Pepe"
-  
-  
-  //Uso de Accesor Y Mutator
   
   var pepe = new Persona("Pepe", 12)
 
